1607ca92b62976d46acba99c423a250e5c9adaba
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-25 21:22:05 +0200

    Fixed container heights, corrected background color palette. Started dividing into containers and columns

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="site_header">
-        SiteHeader
+        <!-- Container principale -->
+        <div class="container-xl p-0 flex_cent h-100 bordo">
+            <div class="row h-75 p-0 w-100 bordo">
+                A
+            </div>
+        </div>
     </div>
 </template>
 
