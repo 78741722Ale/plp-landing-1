f79af2080c6d1cd546fecfc90bbe40b6f3b81a37
78741722Ale <pecorillaalessandro@gmail.com>
2022-06-02 15:21:17 +0200

    Added dynamic content to the Image component.
    Photo detected by lorem picsum causes error during v-for.

--- src/components/main/ImageComponent.vue
+++ src/components/main/ImageComponent.vue
@@ -1,62 +1,35 @@
 <template>
     <div id="image_component">
         <!-- Container principale -->
-        <div class="container-xl p-0 flex_cent h-100">
-            <div class="row h_85 p-0 d-flex justify-content-start align-items-center flex-nowrap flex-column w-100">
+        <div :class="container_class">
+            <div :class="row_class">
                 <!-- Section Name -->
                 <div class="col-3 p-1">
-                    <h6 class="mb-0">Image</h6>
+                    <h6 class="mb-0">{{section_title}}</h6>
                 </div>
                 <!-- Title&Subtitle -->
                 <div class="col-12 h-25 mb-5 flex_cent flex-column">
                     <!-- Title -->
-                    <h2 class="w_60 mt-1 mb-0 pb-3">Add a reason to look at these images</h2>
+                    <h2 class="w_60 mt-1 mb-0 pb-3">{{title}}</h2>
                     <!-- Subtitle -->
-                    <span class="w-100">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos ea facilis vero, minima doloribus 
-                        quia reprehenderit modi rerum inventore aliquam quam velit ut perspiciatis fugiat accusamus iste 
-                        sapiente vel? Quidem molestiae veritatis rerum dicta laboriosam cumque quisquam cum delectus ipsam.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio vel repellendus dolorem assumenda maiores dignissimos?
-                    </span>
+                    <span class="w-100">{{subtitle}}</span>
                 </div>
                 <!-- Three Cards -->
                 <div class="col-12 h_60 p-0">
                     <!-- Sono da Ripetere (le col) in V-for -->
-                    <div class="row-cols-3 gap-4 w-100 h-100 flex_cent m-0 p-0">
+                    <div :class="row_card_class">
                         <!-- First - da Ripetere (le col) in V-for -->
-                        <div class="col-auto card_zone w_32 h-100 p-0 flex-column flex-cent">
+                        <div v-for="(element, index) in card_component" :key="index" :class="single_card_class">
                             <!-- Upper Zone - image -->
                             <div class="col-12 h-75 p-0 m-0">
-                                <img src="https://picsum.photos/200" alt="Image">
+                                <img :src="element.img" :alt="element.desc">
                             </div>
                             <!-- Under Zone - Text -->
-                            <div class="col-12 pb-3 h-25 flex-column d-flex justify-content-end align-items-start">
-                                <h5>Add a catchy headline</h5>
-                                <span>Saggitis non lectus et</span>
-                            </div>
-                        </div>
-                        <!-- Second -->
-                        <div class="col-auto card_zone w_32 h-100 p-0 flex-column flex-cent">
-                            <!-- Upper Zone - image -->
-                            <div class="col-12 h-75 p-0 m-0">
-                                <img src="https://picsum.photos/200" alt="Image">
-                            </div>
-                            <!-- Under Zone - Text -->
-                            <div class="col-12 pb-3 h-25 flex-column d-flex justify-content-end align-items-start">
-                                <h5>Add a catchy headline</h5>
-                                <span>Saggitis non lectus et</span>
-                            </div>
-                        </div>                        
-                        <!-- Third -->
-                        <div class="col-auto card_zone w_32 h-100 p-0 flex-column flex-cent">
-                            <!-- Upper Zone - image -->
-                            <div class="col-12 h-75 p-0 m-0">
-                                <img src="https://picsum.photos/200" alt="Image">
-                            </div>
-                            <!-- Under Zone - Text -->
-                            <div class="col-12 pb-3 h-25 flex-column d-flex justify-content-end align-items-start">
-                                <h5>Add a catchy headline</h5>
-                                <span>Saggitis non lectus et</span>
+                            <div :class="card_under_zone">
+                                <!-- Title -->
+                                <h5>{{element.title}}</h5>
+                                <!-- Subtitle -->
+                                <span>{{element.subtitle}}</span>
                             </div>
                         </div>
                     </div>
@@ -68,7 +41,46 @@
 
 <script>
 export default {
-    name:"ImageComponent"
+    name:"ImageComponent",
+    data() {
+        return {
+            /* Classi dinamiche */
+            container_class: "container-xl p-0 flex_cent h-100",
+            row_class: "row h_85 image_zone w-100",
+            row_card_class :"row-cols-3 gap-4 w-100 h-100 flex_cent m-0 p-0",
+            single_card_class : "col-auto card_zone w_32 h-100 p-0 flex-column flex-cent",
+            card_under_zone : "col-12 pb-3 h-25 flex-column d-flex justify-content-end align-items-start",
+            /* Contenuto Dinamico */
+            section_title : "Image",
+            title:"Add a reason to look at these images",
+            subtitle:" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos ea facilis vero, minima doloribus quia reprehenderit modi rerum inventore aliquam quam velit ut perspiciatis fugiat accusamus iste sapiente vel? Quidem molestiae veritatis rerum dicta laboriosam cumque quisquam cum delectus ipsam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio vel repellendus dolorem assumenda maiores dignissimos?",
+            /* Array di oggetto */
+            card_component : 
+            [   
+                /* Prima Card */
+                {
+                    img: require("@/assets/img/card_img.jpg"),
+                    desc: "Card Image",
+                    title: "Add a catchy headline",
+                    subtitle: "Saggitis non lectus et"
+                },
+                /* Seconda Card */
+                {
+                    img: require("@/assets/img/card_img.jpg"),
+                    desc: "Card Image",
+                    title: "Add a catchy headline",
+                    subtitle: "Saggitis non lectus et"
+                },
+                /* Terza Card */
+                {
+                    img: require("@/assets/img/card_img.jpg"),
+                    desc: "Card Image",
+                    title: "Add a catchy headline",
+                    subtitle: "Saggitis non lectus et"
+                },
+            ]
+        }
+    },
 }
 </script>
 
