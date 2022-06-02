53a92020d6e2dc77f122a18aaac735fb5ca26178
78741722Ale <pecorillaalessandro@gmail.com>
2022-06-02 11:08:02 +0200

    Added (non-dynamic) content to the siteheader.
    Added styling to the header sheet.

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -2,25 +2,28 @@
     <div id="site_header">
         <!-- Container principale -->
         <div class="container-xl p-0 flex_cent h-100">
-            <div class="row h_85 p-0 w-100 bordo flex-column d-flex justify-content-start align-items-center">
-                <!-- BrandName -->
-                <div class="col-3 bg_try p-2 mb-5">Brand name goes here</div>
-                <!-- Title&Subtitle -->
-                <div class="col-10 bg_try p-3 mb-5">
-                    <h1>Title goes here</h1>
-                    <h5>Subtitles goes here</h5>
+            <div class="row h-100 p-0 w-100 flex-column flex_cent">
+                <!-- BrandName (completo ma non statico) -->
+                <div class="col-3 p-1 mb-4">
+                    <h3 class="mb-0">BrandName</h3> 
                 </div>
-                <!-- Call To Action -this is a component, even for Footer site -->
-                <div class="col-6 p-4 flex_cent h_60">
-                    <form class="h-100 w-75 bg_try flex_cent flex-column bordo" action="#">
+                <!-- Title&Subtitle -->
+                <div class="col-10 p-3 mb-3">
+                    <h1>What's your main value proposition?</h1>
+                    <span>Suspendisse eget condimentum mi. Aenean vehicula risus quis hendrerit pulvinar</span>
+                </div>
+                <!-- Call To Action -->
+                <div class="col-6 p-5 flex_cent h-50">
+                    <!-- this is a component, even for Footer site -->
+                    <form class="h-100 w-75 flex_cent flex-column" action="#">
                         <!-- Call to action Title -->
-                        <h5 class="mb-5 mt-5">Call to action title goes here</h5>
+                        <h5 class="mb-5 w-75 mt-1">Let your visitor know what they're getting.</h5>
                         <!-- Name goes Here -->
-                        <input type="text" class="form-control mb-3 w-75 p-2" id="exampleFormControlInput1" placeholder="Name goes Here">
+                        <input type="text" class="form-control mb-3 p-2" placeholder="Name">
                         <!-- Email goes Here -->
-                        <input type="email" class="form-control mb-3 w-75 p-2" id="exampleFormControlInput1" placeholder="Email goes Here">
+                        <input type="email" class="form-control mb-4 p-2" placeholder="Email">
                         <!-- Button Call To Action -->
-                        <button class="btn-primary p-1 w-75">Call to action</button>
+                        <button class="p-2">Call to action</button>
                     </form>
                 </div>
             </div>
