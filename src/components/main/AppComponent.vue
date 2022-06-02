1ccf5acff8535df4abceae6b62d2501e5e0f3a25
78741722Ale <pecorillaalessandro@gmail.com>
2022-06-02 15:43:37 +0200

    Added dynamic content to the App component.
    Added new image taken from the web for the smartphone.

--- src/components/main/AppComponent.vue
+++ src/components/main/AppComponent.vue
@@ -1,91 +1,57 @@
 <template>
     <div id="app_component">
-        <!-- Container principale -->
-        <div class="container-xl p-0 flex_cent h-100">
-            <div class="row h_85 p-0 d-flex justify-content-start align-items-center flex-nowrap flex-column w-100">
+        <!-- Container -->
+        <div :class="app_container">
+            <div :class="app_row">
                 <!-- Section Name -->
                 <div class="col-3 p-1">
-                    <h6 class="mb-0">App Download</h6>
+                    <h6 class="mb-0">{{section_title}}</h6>
                 </div>
                 <!-- Title&Subtitle -->
-                <div class="col-12 h-25 mb-5 d-flex justify-content-start align-items-center flex-column">
+                <div :class="title_class">
                     <!-- Title -->
-                    <h2 class="w_60 mt-1 mb-0 pb-3">Provide a reason to download your app right now</h2>
+                    <h2 class="w_60 mt-1 mb-0 pb-3">{{title}}</h2>
                     <!-- Subtitle -->
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati temporibus assumenda porro beatae voluptatem, rerum, consequuntur 
-                        animi quia architecto debitis iure facilis quaerat minus culpa labore perferendis nostrum, doloremque asperiores cupiditate! Necessitatibus 
-                        architecto facilis sequi, dolores eius iure harum et quaerat odit distinctio veniam eaque dolorum vero voluptatum enim quidem sit qui repudiandae.
-                    </span>
+                    <span>{{subtitle}}</span>
                 </div>
-                <!-- Tutta la zona dello smartphone centrale -->
-                <!-- Valuta un nuovo componente -->
+                <!-- Smartphone&benefit zone -->
                 <div class="col-12 h_60 p-0">
-                    <!-- Row Container di tutto quanto -->
-                    <div class="row-cols-3 gap-5 w-100 h-100 flex_cent m-0 p-0">
-                        <!-- First - Zone 1/3 ( pensalo come un v-for ) -->
+                    <div :class="benefit_row">
+                        <!-- First - Zone 1/3 -->
                         <div class="col-auto zone_1 benefit_zone">
-                            <!-- Col Interne (in v-for) - zona 1 -->
-                            <div class="col-12 h_30 flex_end flex-column">
-                                <!-- Icon col  -->
+                            <!-- Internal col -->
+                            <div v-for="(element, index) in zone_1" :key="index" :class="benefit_col_left">
+                                <!-- Number of istance  -->
                                 <div class="col-3 px-2 h-50 flex_end flex-column">
-                                    <div class="square"><span>1</span></div>
+                                    <div class="square"><span>{{element.number}}</span></div>
                                 </div>
                                 <!-- Text&title col  -->
                                 <div class="col-9 h-50 flex_end flex-column">
                                     <!-- Title -->
-                                    <span class="span_title">Add a benefit of using your app</span>
+                                    <span class="span_title">{{element.title}}</span>
                                     <!-- Subtitle -->
-                                    <span class="span_text">Praesent imperdiet sollicitudin tellus, non tincidunt ipsum gravida ut.</span>
-                                </div>
-                            </div>
-                            <!-- Col Interne (in v-for) - zona 3 -->
-                            <div class="col-12 h_30 flex_end flex-column">
-                                <!-- Icon col  -->
-                                <div class="col-3 px-2 h-50 flex_end flex-column">
-                                    <div class="square"><span>3</span></div>
-                                </div>
-                                <!-- Text&title col  -->
-                                <div class="col-9 h-50 flex_end flex-column">
-                                    <!-- Title -->
-                                    <span class="span_title">Add a benefit of using your app</span>
-                                    <!-- Subtitle -->
-                                    <span class="span_text">Praesent imperdiet sollicitudin tellus, non tincidunt ipsum gravida ut.</span>
+                                    <span class="span_text">{{element.subtitle}}</span>
                                 </div>
                             </div>
                         </div>
                         <!-- Second - Zona Smartphone -->
                         <div class="col-auto p-2 zone_2 w-25 h-100">
-                            <img src="https://picsum.photos/500/800" alt="Smartphone Image">
+                            <img :src="smartphone_img" :alt="smartphone_alt">
                         </div>
-                        <!-- Third - Zone 2/4 ( Pensalo come un v-for) -->
+                        <!-- Third - Zone 2/4 -->
                         <div class="col-auto zone_3 benefit_zone">
-                            <!-- Col Interne (in v-for) - zona 3 -->
-                            <div class="col-12 h_30 flex_start flex-column">
-                                <!-- Icon col  -->
+                            <!-- Internal col  -->
+                            <div v-for="(element, index) in zone_2" :key="index" :class="benefit_col_rigth">
+                                <!-- Number of istance  -->
                                 <div class="col-3 px-2 h-50 flex_start flex-column">
-                                    <div class="square"><span>2</span></div>
+                                    <div class="square"><span>{{element.number}}</span></div>
                                 </div>
                                 <!-- Text&title col  -->
                                 <div class="col-9 h-50 flex_start flex-column">
                                     <!-- Title -->
-                                    <span class="span_title">Add a benefit of using your app</span>
+                                    <span class="span_title">{{element.title}}</span>
                                     <!-- Subtitle -->
-                                    <span class="span_text">Praesent imperdiet sollicitudin tellus, non tincidunt ipsum gravida ut.</span>
-                                </div>
-                            </div>
-                            <!-- Col Interne (in v-for) - zona 4 -->
-                            <div class="col-12 h_30 flex_start flex-column">
-                                <!-- Icon col  -->
-                                <div class="col-3 px-2 h-50 flex_start flex-column">
-                                    <div class="square"><span>4</span></div>
-                                </div>
-                                <!-- Text&title col  -->
-                                <div class="col-9 h-50 flex_start flex-column">
-                                    <!-- Title -->
-                                    <span class="span_title">Add a benefit of using your app</span>
-                                    <!-- Subtitle -->
-                                    <span class="span_text">Praesent imperdiet sollicitudin tellus, non tincidunt ipsum gravida ut.</span>
+                                    <span class="span_text">{{element.subtitle}}</span>
                                 </div>
                             </div>
                         </div>
@@ -98,7 +64,53 @@
 
 <script>
 export default {
-    name:"AppComponent"
+    name:"AppComponent",
+    data() {
+        return {
+            /* Classi Dinamiche */
+            app_container : "container-xl p-0 flex_cent h-100",
+            app_row : "row h_85 app_zone w-100",
+            title_class : "col-12 h-25 mb-5 d-flex justify-content-start align-items-center flex-column",
+            benefit_row : "row-cols-3 gap-5 w-100 h-100 flex_cent m-0 p-0",
+            benefit_col_left : "col-12 h_30 flex_end flex-column",
+            benefit_col_rigth : "col-12 h_30 flex_start flex-column",
+            /* Contenuto Dinamico */
+            smartphone_img : require("@/assets/img/smartphone.jpg"),
+            smartphone_alt : "Smartphone Image",
+            section_title : "App Download",
+            title : "Provide a reason to download your app right now",
+            subtitle : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati temporibus assumenda porro beatae voluptatem, rerum, consequuntur animi quia architecto debitis iure facilis quaerat minus culpa labore perferendis nostrum, doloremque asperiores cupiditate! Necessitatibus architecto facilis sequi, dolores eius iure harum et quaerat odit distinctio veniam eaque dolorum vero voluptatum enim quidem sit qui repudiandae.",
+            /* Array di oggetti */
+            zone_1 : [
+                /* Upper (1) */
+                {
+                    number: "1",
+                    title:"Add a benefit of using your app",
+                    subtitle: "Praesent imperdiet sollicitudin tellus, non tincidunt ipsum gravida ut.",
+                },
+                /* Under (3) */
+                {
+                    number: "3",
+                    title:"Add another benefit",
+                    subtitle: "Praesent imperdiet sollicitudin tellus, non tincidunt ipsum gravida ut.",
+                },
+            ],
+            zone_2 : [
+                /* Upper (2) */
+                {
+                    number: "2",
+                    title:"Add another benefit",
+                    subtitle: "Praesent imperdiet sollicitudin tellus, non tincidunt ipsum gravida ut.",
+                },
+                /* Under (4) */
+                {
+                    number: "3",
+                    title:"Add another benefit",
+                    subtitle: "Praesent imperdiet sollicitudin tellus, non tincidunt ipsum gravida ut.",
+                },
+            ],
+        }
+    },
 }
 </script>
 
